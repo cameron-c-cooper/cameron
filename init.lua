818a31013eb94903e905fca0f2d7566299d37f94
Cameron Cooper <cameron-c-cooper@protonmail.com>
2025-08-11 07:56:27 -0500

    changed version of nvim-treesitter from main to master
    For whatever reason, the modern version does not actually install
    nvim-treesitter.configs which i need

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ vim.pack.add({
 	{ src = "https://github.com/EdenEast/nightfox.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
